3f158f5957a202a37bf1b2a34b8f70905be0a351
Paulo Telles <ptelles020@gmail.com>
2024-03-14 13:36:22 -0300

    add componente home/ enviroment/ service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet
+  ],
+  providers:[
+    HttpClient
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
