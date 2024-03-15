17062a465c86c77a5c3b9dc41f3a992ca815caaf
Paulo Telles <ptelles020@gmail.com>
2024-03-14 21:31:39 -0300

    estilização da tool bar

--- src/app/shared/tool-bar/tool-bar.component.ts
+++ src/app/shared/tool-bar/tool-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
 
 
@@ -7,7 +8,8 @@ import { Router } from '@angular/router';
   selector: 'app-tool-bar',
   standalone: true,
   imports: [
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './tool-bar.component.html',
   styleUrl: './tool-bar.component.scss'
