861506982589be65f98f307711d81657161370e4
Paulo Telles <ptelles040@gmail.com>
2024-03-19 11:52:22 -0300

    ajustando form para anunciar veiculo

--- src/app/components/page/users/users.component.ts
+++ src/app/components/page/users/users.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ToolBarComponent } from '../../../shared/tool-bar/tool-bar.component';
+import { Car } from '../../../models/car/car';
 
 @Component({
   selector: 'app-users',
@@ -10,6 +11,12 @@ import { ToolBarComponent } from '../../../shared/tool-bar/tool-bar.component';
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
-export class UsersComponent{
+export class UsersComponent implements OnInit{
 
+  public userName!: string;
+
+  ngOnInit(): void {
+    localStorage.getItem('userName');
+    this.userName = localStorage.getItem('userName')?.toUpperCase() as string;
+  };
 }
